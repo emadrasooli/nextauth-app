7d2c4e51e8c7a91240d54d0be9d90ffbb3b98bbd
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-11-16 16:40:24 +0430

    2-Setup-HomePage

---
+++ components/ui/auth/card-wrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { BackButton } from "./back-button";
+import { Header } from "./header";
+import { Social } from "./social";
+
+interface CardWrapperProps {
+    children: React.ReactNode;
+    headerLable: string;
+    backButtonLable: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+};
+
+export const CardWrapper = ({
+    children,
+    headerLable,
+    backButtonLable,
+    backButtonHref,
+    showSocial
+}: CardWrapperProps) => {
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerLable}/>
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton 
+                    label={backButtonLable}
+                    href={backButtonHref}
+                />
+            </CardFooter>
+        </Card>
+    )
+}
---
+++ components/ui/auth/login-form.tsx
@@ -0,0 +1,14 @@
+import { CardWrapper } from "./card-wrapper"
+
+export const LoginForm = () => {
+    return (
+        <CardWrapper 
+            headerLable="Welcome back"
+            backButtonLable="Don't have an account?"
+            backButtonHref="/auth/register"
+            showSocial
+        >
+            Login Form!
+        </CardWrapper>
+    )
+}
