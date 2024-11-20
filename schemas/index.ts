a545d046e7052594dce66ff6884bcad13691d91e
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-11-20 16:29:25 +0430

    5-Setup-Database-&-Prisma

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { X } from "lucide-react";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
@@ -6,5 +7,17 @@ export const LoginSchema = z.object({
     }),
     password: z.string().min(1, {
         message: "Password is required"
+    })
+})
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(6, {
+        message: "Minimun 6 characters required"
+    }),
+    name: z.string().min(1, {
+        message: "Name is required"
     })
 })
