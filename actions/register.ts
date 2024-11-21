35d9e511c23703484b75455e13b3734ee2220207
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-11-21 11:10:11 +0430

    6-Setup-Create-User

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -17,11 +18,7 @@ export const Register = async (values: z.infer<typeof RegisterSchema>) => {
     const { email, password, name } = validatedFields.data;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existingUser = await db.user.findUnique({
-        where: {
-            email,
-        }
-    });
+    const existingUser = await getUserByEmail(email)
 
     if(existingUser) {
         return { error: "Email already in use!"}
