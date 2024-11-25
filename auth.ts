9c5bcbef561bca9ee2c49d86d6b8a2e3d79fed06
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-11-25 16:15:38 +0430

    9-Setup-OAuth

--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 
 import { db } from "@/lib/db"
 import authConfig from "./auth.config"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
 
 export const { 
   handlers: { GET, POST }, 
@@ -15,9 +17,20 @@ export const {
       if(token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
       return session;
     },
     async jwt({ token }) {
+      if(!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+
+      token.role = existingUser.role;
       
       return token;
     }
