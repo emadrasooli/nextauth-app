4c62cc4a419d5efb19ab36039e2b865853ab62e8
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-12-01 16:08:18 +0430

    15-Setup-Reset-Form

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,12 @@
 import { X } from "lucide-react";
 import * as z from "zod";
 
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+        message: "Minimun 6 characters required"
+    }),
+})
+
 export const ResetSchema = z.object({
     email: z.string().email({
         message: "Email is required"
