584b5a2fd58cb3e2328f1b6af1ca37a50bc26d8e
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-12-03 14:42:21 +0430

    18-Setup-Dashboard-User

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/auth/user-button";
 
 export const Navbar = () => {
     const pathname = usePathname();
@@ -44,7 +45,7 @@ export const Navbar = () => {
                     </Link>
                 </Button>
             </div>
-            <p>User Button</p>
+            <UserButton />
         </div>
     )
 }
